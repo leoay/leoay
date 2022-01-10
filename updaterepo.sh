565495572e5108f2135255b7d6ef9c190a917bc9
leoay <leoay23@hotmail.com>
2022-01-10 10:23:37 +0800

    update date

--- updaterepo.sh
+++ updaterepo.sh
@@ -5,6 +5,6 @@ git add --all
 git commit -m "update date"
 
 # 推送
-git push --force --quiet "https://leoay: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@github.com/leoay/leoay.git" source:source
+#git push --force --quiet "https://leoay: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@github.com/leoay/leoay.git" source:source
 
 git push origin main
