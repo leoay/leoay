ce0d3ca927b6ff25365ff1b1dd3627f795e173b8
leoay <leoay23@hotmail.com>
2022-01-10 09:54:53 +0800

    update date

--- updaterepo.sh
+++ updaterepo.sh
@@ -3,5 +3,6 @@ git config --global user.email "leoay23@hotmail.com"
 
 git add --all
 git commit -m "update date"
-git pull origin main
-git push origin main
+
+# 推送
+git push --force --quiet "https://${{ secrets.token }}@github.com/leoay/leoay.git" source:source
