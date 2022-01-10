dd088166e2c1bc533f3e19a06b499c73da03e9cb
leoay <leoay23@hotmail.com>
2022-01-10 11:07:09 +0800

    update date

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
-year=$(date +%Y)
-month=$(date +%m)
-day=$(date +%d)
-hour=$(date +%H)
-minu=$(date +%M)
+year=$(TZ=UTC-8 date +%Y)
+month=$(TZ=UTC-8 date +%m)
+day=$(TZ=UTC-8 date +%d)
+hour=$(TZ=UTC-8 date +%H)
+minu=$(TZ=UTC-8 date +%M)
 
 sed -i "15c\# \今天是公元$year年$month月$day日 $hour:$minu" README.md
