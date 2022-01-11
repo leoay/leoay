047964d84d28f5f11d6f86ee1dba4d07693759f0
leoay <leoay23@hotmail.com>
2022-01-11 09:43:01 +0800

    update date

--- test.sh
+++ test.sh
@@ -8,6 +8,6 @@ minu=$(TZ=UTC-8 date +%M)
 
 sed -i "15c\# \今天是公元$year年$month月$day日 $hour:$minu" README.md
 
-echo "end update date..." 今天是公元$year年$month月$day日 $hour:$minu
+echo "end update date..." 现在是公元$year年$month月$day日 $hour:$minu
 
 sh ./updaterepo.sh
