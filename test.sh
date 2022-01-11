4b9b5f765b6d9f4cb29361d3ea26c791fc29d66c
leoay <leoay23@hotmail.com>
2022-01-11 09:50:02 +0800

    update date

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ day=$(TZ=UTC-8 date +%d)
 hour=$(TZ=UTC-8 date +%H)
 minu=$(TZ=UTC-8 date +%M)
 
-sed -i "15c\# \今天是公元$year年$month月$day日 $hour:$minu" README.md
+sed -i "15c\# \现在是公元$year年$month月$day日 $hour:$minu" README.md
 
 echo "end update date..." 现在是公元$year年$month月$day日 $hour:$minu
 
